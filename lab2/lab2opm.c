491fcb02d500adb3b8ed8acdf2d7285a16225554
G4tLan <1043882@students.wits.ac.za>
2018-03-08 16:57:12 +0200

    lab2 with results

--- lab2/lab2opm.c
+++ lab2/lab2opm.c
@@ -5,9 +5,6 @@
 #include <sys/time.h>
 
 
-#define NUM_THREADS 128
-#define MATRIX_DIM 500
-
 void swap(int* A, int* B){
 	
 	int temp = *A;
@@ -51,42 +48,44 @@ void fillMatrix(int* matrix, int No){
 
 int main(void)
 {
-    
+    FILE* resultsFile = fopen("resultsOPM.txt","w");
+	if (resultsFile  == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	
 	struct timeval start, stop;
 	double secs = 0;
-	long int No = 0;
+	int No = 0;
 	int numThreads = 0;
 	
-	printf("insert <Matrix dimensions>\n");
-	scanf("%ld",&No);
-	
-	int* Pr = (int*)calloc(No*No,sizeof(int));
-	
-	fillMatrix(Pr,No);
-	
-	
-	if(No < 10)
-		Print(Pr,No);
-	for(int loop1 = 0; loop1 < 6; loop1++){
-		numThreads = pow(2,loop1 + 2 );
-		gettimeofday(&start, NULL);
-		
-		#pragma omp parallel num_threads(numThreads)
-		{
-			#pragma omp for
-			for(int index = 0; index < No; index++){
-				transposeFromDiagonal(Pr + (No + 1)*index,index, No); 
+	for(int matrixSize = 0; matrixSize <= 13; matrixSize++ ){
+		No = pow(2,matrixSize);
+		int* Pr = (int*)calloc(No*No,sizeof(int));
+		fillMatrix(Pr,No);
+		fprintf(resultsFile, "Matrix Size %d \n", No);
+		for(int loop1 = 0; loop1 < 6; loop1++){
+			numThreads = pow(2,loop1 + 2 );
+			gettimeofday(&start, NULL);
+			
+			#pragma omp parallel num_threads(numThreads)
+			{
+				#pragma omp for
+				for(int index = 0; index < No; index++){
+					transposeFromDiagonal(Pr + (No + 1)*index,index, No); 
+				}
 			}
+			gettimeofday(&stop, NULL);
+				
+			
+			secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+			//printf("time taken %f with threads %d \n",secs,numThreads);
+			fprintf(resultsFile,"%f \t %d \n",secs,numThreads);
 		}
-		gettimeofday(&stop, NULL);
-		if(No < 10)
-			Print(Pr,No);	
-		
-		secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-		printf("time taken %f with threads %d \n",secs,numThreads);
+		free(Pr);
 	}
-	free(Pr);
-	
+	fclose(resultsFile);
 	return 0;
 }
 
--- lab2/lab2pThread.c
+++ lab2/lab2pThread.c
@@ -20,16 +20,16 @@ void *p_transpose(void *arg)
 			
 	//used for switching
 	int *ptr =  arg_Struct->ptr;
-	int limit=   arg_Struct->limit;   
+	int limit1=   arg_Struct->limit;   
 	int i = arg_Struct->ID ;
 	int pos = 0 ;
 	int pos1 = 0 ;
 	for(int j = 0 ; j < i ; j++)
 	{
 		int temp=0;
-		pos = i*limit + j ;
+		pos = i*limit1 + j ;
 		temp = *(ptr+pos) ; 
-		pos1 = j*limit + i ; 
+		pos1 = j*limit1 + i ; 
 		*(ptr+pos) = *(ptr+pos1) ; 
 		*(ptr+pos1) = temp;
 	}
@@ -58,53 +58,62 @@ void Print(int* squareMatrix, int n){
 
 int main(void)
 {
+	FILE* resultsFile = fopen("resultspThread.txt","w");
+	if (resultsFile  == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
 	struct timeval start, stop;
-	long int Dimen =0 ;
+	long Dimen =0 ;
 	double secs = 0 ;
 	int numThreads = 0;
 	
 	
-	printf("insert <Matrix dimensions>\n");
-	scanf("%ld",&Dimen);
-	
-	int *ptr =(int*)calloc(Dimen*Dimen,sizeof(int)); 
-	fillMatrix(ptr, Dimen) ; 
-	pthread_t tids[numThreads] ; 
 	struct arrayInfo args[Dimen];
+	int loop1 = 0;
+	int tepp= Dimen ; 
 	
-	for(int loop1 = 0; loop1 < 6; loop1++){
-		numThreads = pow(2,loop1 + 2 );
-		gettimeofday(&start, NULL);	
-		for(int i = 0 ; i < Dimen ; i++ )
-		{
-			args[i].ID = i ; 
-			args[i].limit = Dimen ; 
-			args[i].ptr= ptr;  
-			int temp = i % numThreads;
-			pthread_create(&tids[temp], NULL, p_transpose , &args[i]);
-		}
-		
-
-		
-		for(int i = 0 ; i <  numThreads ; i++ )
-		{
-			if(numThreads <= Dimen)
+	for(int matrixSize = 0; matrixSize <= 13; matrixSize++ ){
+		Dimen = pow(2,matrixSize);
+		int* ptr = (int*)calloc(Dimen*Dimen,sizeof(int));
+		fillMatrix(ptr,Dimen);
+		fprintf(resultsFile, "Matrix Size %d \n", Dimen);
+		for(loop1 = 0; loop1 < 6; loop1++){
+			numThreads = pow(2,loop1 + 2 );
+			pthread_t* tids = calloc(numThreads, sizeof(pthread_t)) ; 
+			gettimeofday(&start, NULL);	
+			for(int i = 0 ; i < tepp ; i++ )
 			{
-			
-			pthread_join(tids[i], NULL) ; 
-			} else {
-				pthread_join(tids[i%Dimen], NULL) ; 
+				
+				args[i].ID = i ; 
+				args[i].limit = Dimen ; 
+				args[i].ptr= ptr;  
+				int temp = i % numThreads;
+				pthread_create(&tids[temp], NULL, p_transpose , &args[i]);
 			}
 			
-		}
-		gettimeofday(&stop, NULL);
 
-		
-		secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-		printf("time taken %f with threads %d \n",secs,numThreads);
+			
+			for(int i = 0 ; i <  numThreads ; i++ )
+			{
+				if(numThreads <= tepp)
+				{
+				
+				pthread_join(tids[i], NULL) ; 
+				} else {
+					pthread_join(tids[i%Dimen], NULL) ; 
+				}
+				
+			}
+			gettimeofday(&stop, NULL);
+
+			
+			secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+			fprintf(resultsFile,"%f \t %d \n",secs,numThreads);
+		}
+		free(ptr);
 	}
-	
-	free(ptr);
 	
 	pthread_exit(0);
 }
